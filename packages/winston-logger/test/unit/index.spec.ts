0f1806674978a4040d1b2164bc8ce1c35b87dc89
fengcang <fengcang@blued.com>
2021-05-21 20:43:52 +0800

    kafka日志

--- packages/winston-logger/test/unit/index.spec.ts
+++ packages/winston-logger/test/unit/index.spec.ts
@@ -3,11 +3,12 @@ import Logger from '../../src/index'
 
 describe('normal', () => {
   it(`output logs`, done => {
-    const loggerClient = new Logger('logs', new Map())
+    const loggerClient = new Logger('logs', new Map(), true)
     try {
       const logger = loggerClient.getLogger('test')
       logger.access({ msg: 'test' })
       logger.error(new Error('test error'), { msg: 'test' })
+      logger.info({ msg: 'testi info' })
     } catch (e) {
       console.log(e)
     }
